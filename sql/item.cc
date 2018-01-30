c2b2a277dd6457a26b73f0986ebbf3b790e0cc50
Sergei Golubchik <serg@mariadb.org>
2018-01-30 22:37:42 +0100

    cleanup: Item_trigger_field::setup_field()
    Don't mess with thd->mark_used_columns to prevent find_field_in_table()
    from marking columns. Use TABLE::find_field_by_name() directly.

--- sql/item.cc
+++ sql/item.cc
@@ -9527,15 +9527,13 @@ void Item_trigger_field::setup_field(THD *thd, TABLE *table,
     So instead we do it in Table_triggers_list::mark_fields_used()
     method which is called during execution of these statements.
   */
-  enum_mark_columns save_mark_used_columns= thd->mark_used_columns;
-  thd->mark_used_columns= MARK_COLUMNS_NONE;
   /*
     Try to find field by its name and if it will be found
     set field_idx properly.
   */
-  (void)find_field_in_table(thd, table, field_name.str, field_name.length,
-                            0, &field_idx);
-  thd->mark_used_columns= save_mark_used_columns;
+  Field *f= table->find_field_by_name(&field_name);
+  if (f)
+    field_idx= f->field_index;
   triggers= table->triggers;
   table_grants= table_grant_info;
 }
